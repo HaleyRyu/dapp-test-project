298014b08c0d3211de2ed4ea776d6474676c2ae4
Developer Cookie <developercookie28@gmail.com>
2022-02-07 22:22:43 +0900

    Add: animalTypes mapping
    민팅시 tokenId 에 animalType 을 매핑 (타입값은 1-5 사이의 숫자 랜덤 추출)

--- MintAnimalToken.sol
+++ MintAnimalToken.sol
@@ -7,8 +7,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MintAnimalToken is ERC721Enumerable {
     constructor() ERC721("AmiAnimals", "AA") {}
 
+    // animalTokenId => animalType (1-5)
+    mapping(uint256 => uint256) public animalTypes;
+
     function mintAnimalToken() public {
         uint256 animalTokenId = totalSupply() + 1;
+
+        // animalType 랜덤화 (1-5)
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
+
+        animalTypes[animalTokenId] = animalType;
 
         _mint(msg.sender, animalTokenId);
     }
