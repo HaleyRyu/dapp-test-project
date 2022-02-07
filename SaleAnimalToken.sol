fddac0e6dcf27f7b032cff737adec2cfbfd0a405
Developer Cookie <developercookie28@gmail.com>
2022-02-08 00:28:00 +0900

    Add: 구매함수 작성 (purchaseAnimalToken)
    판매등록된 리스트에 있는 토큰을 사용자가 구매 요청할때 사용되는 함수 작성.
    
    Remix 테스트 방법
    1. MintAnimalToken deploy 후 minting
    2. SaleAnimalToken deploy (mintAnimalToken Address 를 인자로 넣어서 transact)
    3. setApprovalForAll 실행 (operator: Sale 컨트랙트 주소, approved: true)
    4. 판매등록: setForSaleAnimalToken (tokenId: 1, price: 10)
    5. onSaleAnimalTokenArray에 index 0을 넣어보면 토큰 1번이 들어가있는걸 확인할 수 있고, animalTokenPrices 에서 1로 조회하면 price 10 으로 판매되고 있는것을 확인할 수 있다.
    6. 구매하기: Account 를 다른 계정으로 변경하고 value: 10 으로 셋팅한뒤, purchaseAnimalToken 함수에 id:1 넣고 transact
    7. ownerOf 에 tokenId 1을 넣어보면 주인이 변경된것을 확인할 수 있다.

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -12,7 +12,7 @@ contract SaleAnimalToken {
     }
 
     // animalTokenId => price
-    mapping(uint256 => uint256) public animalTokenPrices;
+    mapping(uint256 = > uint256) public animalTokenPrices;
 
     // FE에서 어떤 토큰이 판매중인 토큰인지 확인하는데 사용하는 배열.
     uint256[] public onSaleAnimalTokenArray;
@@ -30,5 +30,45 @@ contract SaleAnimalToken {
         animalTokenPrices[_animalTokenId] = _price;
 
         onSaleAnimalTokenArray.push(_animalTokenId);
+    }
+
+    // payable 키워드를 붙여야 polygon network 에서 matic 이 이동하는 함수를 실행할 수 있다.
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        
+        // Caller가 구매하려는 토큰의 주인의 주소값을 불러온다.
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+        // 구매하려는 토큰의 price 가 0보다 같거나 작다면 아직 판매등록 되지 않은 토큰이므로 구매할 수 없다.
+        require(price > 0, "This token is not on sale.");
+
+        // 위 함수를 실행할때 Caller가 보내는 matic 양. price 보다 같거나 크게 보내야 구매할 수 있다.
+        require(price <= msg.value, "Caller sent lower than price.");
+
+        // Caller 가 토큰의 주인과는 다른 사람이어야 구매가 가능하다.
+        require(animalTokenOwner != msg.sender, "Caller is animal token's owner.");
+
+        // msg.sender 에 msg.value 만큼의 양이 토큰 주인에게 전송된다.
+        payable(animalTokenOwner).transfer(msg.value);
+
+        // 구매한 토큰의 주인을 sender 로 변경한다.
+        // 인자: 보내는 사람, 받는 사람, 토큰ID
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+
+        // 팔린 토큰의 price 초기화
+        animalTokenPrices[_animalTokenId] = 0;
+
+        // 판매 등록된 리스트에서 팔린 토큰은 삭제
+        for(uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
+            }
+        }
+    }
+
+    // Frontend 에서 사용할 함수 (판매중인 리스트 사이즈를 가져오는 함수)
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
     }
 }
