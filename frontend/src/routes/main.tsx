d0447f2c907aff808c4c3eb0ff92201289d189ab
Developer Cookie <developercookie28@gmail.com>
2022-02-08 22:12:17 +0900

    Add: frontend files
    프론트엔드 기본 코드, Material UI 추가

---
+++ frontend/src/routes/main.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+const Main: FC = () => {
+    return <Box>Main <Button>Button</Button></Box>;
+};
+
+export default Main;
