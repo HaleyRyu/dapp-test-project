7a6223779adbd8561dd0eee937ba1dd43ba806aa
Developer Cookie <developercookie28@gmail.com>
2022-02-14 22:46:36 +0900

    Update: Smart Contracts 수정
    Remix 에서 테스트하기
    - Mint, Sale Smart Contract 수정했으므로 다시 컴파일하고 deploy 해보기

--- contracts/SaleAnimalToken.sol
+++ contracts/SaleAnimalToken.sol
@@ -71,4 +71,8 @@ contract SaleAnimalToken {
     function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
         return onSaleAnimalTokenArray.length;
     }
+
+    function getAnimalTokenPrice(uint256 _animalTokenId) view public returns (uint256) {
+        return animalTokenPrices[_animalTokenId];
+    }
 }
